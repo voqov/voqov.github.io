f7a2ed5d5f0e5417a513dc37346f3049b88a33c6
고서현 <voqov616@gmail.com>
2023-09-18 15:39:03 +0900

    Update CyclicRotation.java
    Score = 100%
    K >= N 일 경우를 고려하여 몫을 구해서 새 idx로 지정하는 방식으로 수정

--- codingTest/codility/lessons2/CyclicRotation.java
+++ codingTest/codility/lessons2/CyclicRotation.java
@@ -1,21 +1,23 @@
 class Solution {
     public int[] solution(int[] A, int K) {
         // Implement your solution here
-        int length = A.length;
+        int N = A.length;
 
-        if (length == 0 || length == 1 || length == K) {
+        if (N == 0 || N == 1) {
             return A;
         }
 
-        int[] answer = new int[A.length];
+        int[] answer = new int[N];
 
-        for (int idx = 0; idx < length; idx++) {
-            if (idx + K < length) {
+        for (int idx = 0; idx < N; idx++) {
+            if (idx + K < N) {
                 answer[idx + K] = A[idx];
             } else {
-                answer[idx + K - length] = A[idx];
+                int rIdx = (idx + K) % N;
+                answer[rIdx] = A[idx];
             }
         }
+
         return answer;
     }
 }
